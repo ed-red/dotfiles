359c1c62d6f5b9d44e642fa1f2437d6f5470ee8e
Ed|red <43439833+ed-red@users.noreply.github.com>
2023-04-25 12:48:28 -0300

    Update setup.sh

--- setup/ubuntu/setup.sh
+++ setup/ubuntu/setup.sh
@@ -4,7 +4,7 @@ source $DOTFILES/setup/ubuntu/base.sh
 source $DOTFILES/setup/ubuntu/devel.sh
 source $DOTFILES/setup/ubuntu/apps.sh
 source $DOTFILES/setup/ubuntu/fonts.sh
-# source $DOTFILES/setup/ubuntu/terminal.sh
+source $DOTFILES/setup/ubuntu/terminal.sh
 source $DOTFILES/setup/copy_dots.sh
 source $DOTFILES/setup/install_golang.sh
 source $DOTFILES/setup/install_hacktools.sh
